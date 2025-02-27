9264df75d447dd2d50009b1bf75dc5f6790a0b2b
Danik013 <2077647@gmail.com>
2025-02-27 13:05:21 +0300

    Add files via upload

--- basic_03.py
+++ basic_03.py
@@ -17,12 +17,11 @@ text_letter = ("""Привет, %friend_name%! %my_name% приглашает т
 
 Регистрируйся → %website%  
 На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""")
+
 website = "https://dvmn.org/profession-ref-program/2077647/d6NPX/"
 frend_name = "Павел"
 my_name = "Даниил"
-text_letter = text_letter.replace("%friend_name%",(frend_name))
-text_letter = text_letter.replace("%my_name%",(my_name))
-text_letter = text_letter.replace("%website%",(website))
+text_letter = text_letter.replace("%friend_name%",(frend_name), "%my_name%",(my_name), "%website%",(website))
 from_mail = "danik013@yandex.ru"
 to_mail = "2077647@gmail.com"
 subjest_mail = "Приглашение!"
@@ -33,9 +32,11 @@ Subject: {sub}
 Content-Type: text/plain; charset="UTF-8";""".format(fr=from_mail, to=to_mail, sub=subjest_mail)
 letter = "{h}\n\n{t}".format(h=headers_, t=text_letter)
 letter = letter.encode("UTF-8")
+
 load_dotenv()
 mail_login = os.getenv('LOGIN')
 mail_pass = os.getenv('PASSWORD')
+
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
 server.login(mail_login, mail_pass)
 server.sendmail(from_mail, to_mail, letter)

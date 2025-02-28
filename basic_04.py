c59bd191df612f6ebf847bf1e61d811b2ccd1fa3
Danik013 <2077647@gmail.com>
2025-02-28 09:46:52 +0300

    Add files via upload

--- basic_04.py
+++ basic_04.py
@@ -1,25 +1,25 @@
 from PIL import Image
 
 monro = Image.open("monro.jpg")
-red_monro, blue_monro, green_monro = monro.split()
+red_monro, green_monro, blue_monro = monro.split()
 
 red_coord_left = (50, 0, red_monro.width, red_monro.height)
 red_cropped_left = red_monro.crop(red_coord_left)
 red_coord_mid = (25, 0, 671, red_monro.height)
 red_cropped_mid = red_monro.crop(red_coord_mid)
-red_monro_blend = Image.blend(red_cropped_mid, red_cropped_left, 0.2)
+red_monro_blend = Image.blend(red_cropped_mid, red_cropped_left, 0.5)
 
 blue_coord_right = (0, 0, 646, blue_monro.height)
 blue_cropped_right = blue_monro.crop(blue_coord_right)
 blue_coord_mid = (25, 0, 671, blue_monro.height)
 blue_cropped_mid = blue_monro.crop(blue_coord_mid)
-blue_monro_blend = Image.blend(blue_cropped_mid, blue_cropped_right, 0.2)
+blue_monro_blend = Image.blend(blue_cropped_mid, blue_cropped_right, 0.5)
 
 green_coord_mid = (25, 0, 671, green_monro.height)
 green_cropped_mid = green_monro.crop(green_coord_mid)
 
-new_monro = Image.merge("RGB", (red_monro_blend, blue_monro_blend, green_cropped_mid))
+new_monro = Image.merge("RGB", (red_monro_blend, green_cropped_mid, blue_monro_blend))
 new_monro.save("new_monro.jpg")
-avatar = Image.merge("RGB", (red_monro_blend, blue_monro_blend, green_cropped_mid))
+avatar = Image.merge("RGB", (red_monro_blend, green_cropped_mid, blue_monro_blend))
 avatar.thumbnail((80, 80))
 avatar.save("avatar.jpg")
